f1a135656c51cb3abd942a2ba20f21949b51787f
selfsculpted <selfsculpted@selfsculpteds-MacBook-Pro.local>
2022-05-23 15:09:31 +0100

    setup login page with working functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-import LoginForm from './components/loginform'
+import LoginForm from './components/loginform';
+import axios from 'axios';
 function App() {
+  const Login = loginDetails => {
+    axios.post("http://localhost:3002/login", loginDetails).then((res) => {
+      console.log(res)
+    })
+  }
   return (
     <div className="App">
-      <LoginForm />
+      <LoginForm Login={Login} />
     </div>
   );
 }
--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
-function LoginForm() {
-    const [details, setDetails] = useState({ username: '', password: '' });
+function LoginForm({ Login }) {
+    const [loginDetails, setloginDetails] = useState({ username: '', password: '' });
     const submitHandler = e => {
         e.preventDefault();
-        console.log(details)
+        Login(loginDetails)
     }
     return (
         <form onSubmit={submitHandler}>
@@ -14,11 +14,11 @@ function LoginForm() {
                 </h2>
                 <div>
                     <label>Username</label>
-                    <input onChange={e => setDetails({ ...details, username: e.target.value })}></input>
+                    <input onChange={e => setloginDetails({ ...loginDetails, username: e.target.value })}></input>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" onChange={e => setDetails({ ...details, password: e.target.value })}></input>
+                    <input type="password" onChange={e => setloginDetails({ ...loginDetails, password: e.target.value })}></input>
                 </div>
                 <input type="submit"></input>
             </div>
