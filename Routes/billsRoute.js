780319ecfdb61935da977f139c0c67d31d10f3d1
selfsculpted <selfsculpted@selfsculpteds-MacBook-Pro.local>
2022-06-26 04:42:08 +0100

    refactored billsRoute

--- Routes/billsRoute.js
+++ Routes/billsRoute.js
@@ -1,177 +1,172 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const Router = express.Router();
+const router = express.Router();
 const Redis = require("ioredis");
 const redis = new Redis();
 const moment = require("moment");
-const { isAdmin, isAuth } = require('./authRoute');
+const { isAdmin, isAuth } = require("./authRoute");
 
-
-const getUserID = async (username) => {
-    const user = await prisma.customer.findFirst({
-        where: {
-            username: username
-        }
-    })
-    return user.id
-}
-const getSession = async (userSessionID) => {
-    const userData = await redis.get(userSessionID, (error, result) => {
-        return result
-    });
-    return JSON.parse(userData)
+const getUserId = async (username) => {
+  const user = await prisma.customer.findFirst({
+    where: {
+      username: username,
+    },
+  });
+  return user.id;
 };
-const getBills = async (customerID) => {
+const createBill = async (bill) => {
+  try {
+    const newBill = await prisma.bills.create({
+      data: bill[0],
+    });
+    return newBill;
+  } catch (error) {
+    return { error: "Server error! request not fulfilled" };
+  }
+};
+const userBills = async (userId) => {
+  try {
     const bill = await prisma.bills.findMany({
-        where: {
-            customerId: customerID
-        }
-    })
-    return bill
-}
+      where: {
+        customerId: userId,
+      },
+    });
+    return bill;
+  } catch (error) {
+    return { error: "Server error! request not completed" };
+  }
+};
+const updateBill = async (billId, bill) => {
+  try {
+    const updatedBill = await prisma.bills.update({
+      where: {
+        id: Number(billId),
+      },
+      data: bill[0],
+    });
+    return updatedBill;
+  } catch (error) {
+    return { error: "Server error! request not fulfilled!" };
+  }
+};
 const getAllBills = async () => {
-    const allBills = await prisma.bills.findMany()
-    return allBills
-}
-const deleteAllUserBills = async (userID) => {
-    const deletedBills = await prisma.bills.deleteMany({
-        where: {
-            customerId: userID,
-        },
-    })
-    return deletedBills
-}
+  try {
+    const bills = await prisma.bills.findMany();
+    return bills;
+  } catch (error) {
+    return { error: "Server error! request not completed" };
+  }
+};
 const deleteBill = async (billID) => {
-    const deletedBill = await prisma.bills.delete({
-        where: {
-            id: billID,
-        },
-    })
-    return deletedBill
-}
-const addBill = async (bill) => {
-    const addedBill = await prisma.bills.create({
-        data: bill[0]
-    })
-    return addedBill
-}
-const updateBill = async (billID, data) => {
-    console.log(data)
-    await prisma.bills.update({
-        where: {
-            id: Number(billID)
-        },
-        data: data[0]
-    })
-    console.log("success")
-}
+  const deletedBill = await prisma.bills.delete({
+    where: {
+      id: billID,
+    },
+  });
+  return deletedBill;
+};
 const convertDateClient = (bills) => {
-    let newBills = bills.map((bill) => {
-        return ({ ...bill, due_date: bill.due_date ? moment(bill.due_date).format('LL') : "No Due Date", end_date: bill.end_date ? moment(bill.end_date).format('LL') : null })
-    })
-    return newBills
-}
-
+  let newBills = bills.map((bill) => {
+    return {
+      ...bill,
+      due_date: bill.due_date
+        ? moment(bill.due_date).format("LL")
+        : "No Due Date",
+      end_date: bill.end_date ? moment(bill.end_date).format("LL") : null,
+    };
+  });
+  return newBills;
+};
 const convertDateDB = (bills) => {
-    let newBills = bills.map((bill) => {
-        return ({ ...bill, due_date: bill.due_date ? moment(bill.due_date).format() : null, end_date: bill.end_date ? moment(bill.end_date).format() : null })
-    })
-    return newBills
-}
+  let newBills = bills.map((bill) => {
+    return {
+      ...bill,
+      due_date: bill.due_date ? moment(bill.due_date).format() : null,
+      end_date: bill.end_date ? moment(bill.end_date).format() : null,
+    };
+  });
+  return newBills;
+};
+
+const getSession = async (userSessionID) => {
+  const userData = await redis.get(userSessionID, (error, result) => {
+    return result;
+  });
+  return JSON.parse(userData);
+};
+const deleteAllUserBills = async (userID) => {
+  const deletedBills = await prisma.bills.deleteMany({
+    where: {
+      customerId: userID,
+    },
+  });
+  return deletedBills;
+};
+
 const validateBill = (bill) => {
-    //check properties about a bill to see if its a valid
-    if (bill.repeat) {
-        console.log(true)
-    }
-    //1. update the current month and year of each bill
-    //1. check if repeat is true
-    //2. if repeat is true, check if the due date is less than the end_date
+  //check properties about a bill to see if its a valid
+  if (bill.repeat) {
+    console.log(true);
+  }
+  //1. update the current month and year of each bill
+  //1. check if repeat is true
+  //2. if repeat is true, check if the due date is less than the end_date
 
-    return
-}
-Router.get('/', async (req, res) => {
-    const userSessionID = 'sess:' + req.query.sessionID;
-    const response = await getSession(userSessionID);
-    if (!response) {
-        res.status(403);
-        console.log("No session found");
-        res.send("No session found");
-        return
-    }
-    const customerID = await getUserID(response.userid);
-    let bills = await getBills(customerID);
+  return;
+};
 
-    //bills.map((bill) => validateBill(bill));
-
+router.get("/bills", isAuth, async (req, res) => {
+  const userid = await getUserId(req.user.username);
+  let bills = await userBills(userid);
+  if (!bills.error) {
     bills = convertDateClient(bills);
-    res.send(bills)
-})
+    res.status(200).json(bills);
+  } else {
+    res.status(500).json({ message: bills.error });
+  }
+});
+router.post("/bill", isAuth, async (req, res) => {
+  const userid = await getUserId(req.user.username);
+  let bill = req.body.bill;
+  bill.customerId = userid;
+  bill.amount = parseFloat(bill.amount);
+  bill.end_date = moment(bill.end_date).format();
+  bill = convertDateDB([bill]);
+  const newBill = await createBill(bill);
+  if (!newBill.error) {
+    res.status(200).json({ message: "Bill successfully created!" });
+  } else {
+    res.status(500).json({ message: newBill.error });
+  }
+});
+router.put("/bill", isAuth, async (req, res) => {
+  const billId = req.body.data.billID;
+  req.body.data.bill.amount = parseFloat(req.body.data.bill.amount);
+  let bill = convertDateDB([req.body.data.bill]);
+  const updatedBill = await updateBill(billId, bill);
+  if (!updatedBill.error) {
+    res.status(200).json({ message: "Bill updated successfully!" });
+  } else {
+    res.status(500).json({ message: updatedBill.error });
+  }
+});
+router.get("/admin/bills", isAdmin, async (req, res) => {
+  const allBills = await getAllBills();
+  if (!allBills.error) {
+    res.status(200).json(allBills);
+  } else {
+    res.status(500).json({ message: allBills.error });
+  }
+});
+router.delete("/bill", async (req, res) => {
+  const billId = req.body.data.billID;
+  const deletedBill = await deleteBill(billId);
+  if (!deletedBill.error) {
+    res.status(200).json(deletedBill);
+  } else {
+    res.status(500).json({ message: deletedBill.error });
+  }
+});
 
-Router.post('/bill', async (req, res) => {
-    const userSessionID = "sess:" + req.body.userID;
-    const userData = await getSession(userSessionID);
-    const username = userData.userid;
-    const userid = await getUserID(username);
-    let bill = req.body.bill;
-    bill.customerId = userid;
-    bill.amount = parseFloat(bill.amount);
-    bill.end_date = moment(bill.end_date).format();
-    console.log(bill)
-    bill = convertDateDB([bill])
-    const newBill = await addBill(bill);
-    res.send(bill)
-    //res.send(newBill);
-})
-
-Router.put('/bill', async (req, res) => {
-    const usersessionID = "sess:" + req.body.headers.Authorization;
-    console.log(usersessionID)
-    const session = await getSession(usersessionID);
-    const billID = req.body.data.billID;
-    req.body.data.bill.amount = parseFloat(req.body.data.bill.amount);
-    let updatedBill = convertDateDB([req.body.data.bill]);
-
-
-    if (!session) {
-        res.statusCode = 403;
-        return [];
-    }
-
-    try {
-        await updateBill(billID, updatedBill);
-
-
-    } catch (error) {
-        console.error(error);
-        return []
-    }
-    res.send("success");
-})
-
-Router.get('/allBills', async (req, res) => {
-    const allBills = await getAllBills();
-    res.send(allBills)
-})
-Router.delete('/bill', async (req, res) => {
-
-    const usersessionID = "sess:" + req.headers.authorization;
-    console.log(req)
-    const session = await getSession(usersessionID);
-    const billID = req.body.billID;
-    if (!session) {
-        res.statusCode = 403;
-        return [];
-    }
-    try {
-        await deleteBill(billID);
-
-    } catch (error) {
-        console.error(error);
-        return []
-    }
-    res.send("success");
-})
-
-module.exports = Router;
-//module.exports = deleteBills;
+module.exports = router;
